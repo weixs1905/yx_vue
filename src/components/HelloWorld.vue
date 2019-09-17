db667c01afd71b321a7664ed8eae43947b6cfbe3
weixs1905 <weixs1905@163.com>
2019-09-18 00:39:33 +0800

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        num:[]
+      }
+    },
+    mounted(){
+     this.teamlist('参数').then(res =>{
+       if(res.status == 0){
+         let reslut = res.data
+         console.log(reslut)
+       }
+     })
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+ 
+</style>
